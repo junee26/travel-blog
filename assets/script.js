fc106c20c6f8aa2e619955a14db29a57b99acb22
junee26 <sjunee5@gmail.com>
2023-03-15 16:16:48 +0545

    changes

--- assets/script.js
+++ assets/script.js
@@ -4,21 +4,23 @@ form.addEventListener('submit', handleSearch);
 const searchBar = document.querySelector('#searchbar');
 const blogPosts = document.querySelectorAll('.news-snippet');
 
-
-
 function handleSearch(event) {
+ 
+  alert(true);
   const searchTerm = document.querySelector('#searchbar').value;
-  alert(searchTerm);
+
 
   blogPosts.forEach(post => {
     const title = post.querySelector('.news-title').textContent.toLowerCase();
     const content = post.querySelector('.post-content').textContent.toLowerCase();
     const matches = title.includes(searchTerm) || content.includes(searchTerm);
-    alert(post)
     if (matches) {
       post.classList.remove('hidden');
     } else {
+      alert("unmatches");
       post.classList.add('hidden');
+      event.stopImmediatePropagation();
     }
   });
+  return false;
 }
